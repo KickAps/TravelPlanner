07fb60f3463923accf628c219abb967ef9a06b3e
KickAps <flo.berson06@gmail.com>
2023-05-22 13:06:55 +0200

    ajout btn par défaut

--- assets/react/controllers/Steps.jsx
+++ assets/react/controllers/Steps.jsx
@@ -85,6 +85,12 @@ class Steps extends React.Component {
     render() {
         return (
             <div>
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={this.addDiv}
+                >
+                    <i className="fa-solid fa-plus"></i>
+                </button>
                 {this.state.divs.map(div => div)}
             </div>
         );
