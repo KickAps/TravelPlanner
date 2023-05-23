c9ca8398fb99b90b2d0ef52a8faa09340af18c81
KickAps <flo.berson06@gmail.com>
2023-05-23 11:37:13 +0200

    form & style

--- assets/react/controllers/Steps.jsx
+++ assets/react/controllers/Steps.jsx
@@ -15,7 +15,7 @@ class Steps extends React.Component {
         for (const [key, value] of Object.entries(this.state.data)) {
             setTimeout(() => {
                 this.addStep(null, key, value.place);
-            }, key * 500);
+            }, key * 1000);
         }
     }
 
@@ -43,32 +43,76 @@ class Steps extends React.Component {
 
         const newStep = (
             <div key={newStepCount} id={`card_${newStepCount}`}>
-                <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                    <div>
+                <div className="w-4/5 mx-auto my-2 rounded shadow-lg bg-white">
+                    <div className="relative z-10">
                         <button
-                            className="relative top-0 right-0 p-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+                            className="absolute top-0 right-0 pt-2 pr-3 text-gray-500 hover:text-gray-700 focus:outline-none"
                             onClick={() => this.deleteStep(`card_${newStepCount}`)}
                         >
                             <i className="fa-solid fa-xmark"></i>
                         </button>
                     </div>
 
-                    <div className="px-6 py-2">
-                        <div className="font-bold text-xl mb-2">
-                            <span>Étape </span>
+                    <div className="relative p-4">
+                        <div className="absolute -left-16 top-2 font-bold text-lg text-center px-4 p-2 rounded-full border-2 border-gray-300 bg-white">
                             <span className="step_index">{newStepCount}</span>
                         </div>
-                        <input
-                            id={`input_${newStepCount}`}
-                            name={`input_${newStepCount}`}
-                            className="controls pac-input"
-                            type="text"
-                            placeholder="Rechercher..."
-                        />
+                        <form>
+                            <div className="flex mb-2">
+                                <div className="w-full md:w-1/2 px-3 md:mb-0">
+                                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor={`input_${newStepCount}`}>
+                                        Lieux
+                                    </label>
+                                    <input
+                                        id={`input_${newStepCount}`}
+                                        name={`input_${newStepCount}`}
+                                        className="pac-input appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                        type="text"
+                                        placeholder="Rechercher..."
+                                    />
+                                </div>
+                                <div className="w-full md:w-1/2 px-3">
+                                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="date">
+                                        Date
+                                    </label>
+                                    <input
+                                        id="date"
+                                        type="date"
+                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap mb-2">
+                                <div className="w-full px-3">
+                                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="description">
+                                        Description
+                                    </label>
+                                    <textarea
+                                        id="description"
+                                        rows="4"
+                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                        placeholder="Nous allons...">
+                                    </textarea>
+                                </div>
+                            </div>
+                            <div className="flex flex-wrap mb-2">
+                                <div className="w-full px-3">
+                                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="notes">
+                                        Notes
+                                    </label>
+                                    <textarea
+                                        id="notes"
+                                        rows="2"
+                                        className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                        placeholder="Quelques notes...">
+                                    </textarea>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
                 <button
-                    className="btn_add bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="btn_add block bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded mx-auto"
                     onClick={this.addStep}
                 >
                     <i className="fa-solid fa-plus"></i>
@@ -92,7 +136,7 @@ class Steps extends React.Component {
                     input.value = place;
                     google.maps.event.trigger(input, 'focus', {});
                     google.maps.event.trigger(input, 'keydown', { keyCode: 13 });
-                }, 500);
+                }, 1000);
             }
         });
     };
@@ -101,7 +145,7 @@ class Steps extends React.Component {
         return (
             <div>
                 <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    className="bg-blue-500 block hover:bg-blue-700 text-white font-bold py-1 px-3 rounded mx-auto"
                     onClick={this.addStep}
                 >
                     <i className="fa-solid fa-plus"></i>
