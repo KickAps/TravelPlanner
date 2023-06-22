ae9017043f2789871925e76ba613e2e68a29a0a8
KickAps <flo.berson06@gmail.com>
2023-06-22 17:51:37 +0200

    Navbar dropdown mobile

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -7,3 +7,24 @@ export function showUnsaved() {
     document.getElementById("saved_icon").classList.add('hidden');
     document.getElementById("unsaved_icon").classList.remove('hidden');
 }
+
+window.addEventListener('load', function () {
+    const dropdown_btn = document.getElementById("dropdown_btn");
+    const dropdown_menu = document.getElementById("dropdown_menu");
+    const dropdown_close = document.getElementById("dropdown_close");
+
+    dropdown_btn.addEventListener('click', function () {
+        if (dropdown_menu.classList.contains("hidden")) {
+            dropdown_menu.classList.remove("hidden");
+            dropdown_close.classList.remove("hidden");
+        } else {
+            dropdown_menu.classList.add("hidden");
+            dropdown_close.classList.add("hidden");
+        }
+    });
+
+    dropdown_close.addEventListener('click', function () {
+        dropdown_menu.classList.add("hidden");
+        dropdown_close.classList.add("hidden");
+    });
+});
